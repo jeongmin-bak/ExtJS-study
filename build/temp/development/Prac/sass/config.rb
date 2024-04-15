87d51e3ed4c5d9c03c87a8ab285e300733cf8334
jeongmin-bak <jmpark@didim365.com>
2024-04-15 14:59:37 +0900

    init commit

---
+++ build/temp/development/Prac/sass/config.rb
@@ -0,0 +1,2 @@
+Compass.add_project_configuration('../../../../../sass/config.rb')
+cache_path = '/Users/parkjungmin/Desktop/extjs/Prac/build/.sass-cache'
---
+++ build/temp/development/Prac/sencha-compiler/app/full-page-master-bundle.js
@@ -0,0 +1,2 @@
+// @tag full-page
+// @require /Users/parkjungmin/Desktop/extjs/Prac/app.js
